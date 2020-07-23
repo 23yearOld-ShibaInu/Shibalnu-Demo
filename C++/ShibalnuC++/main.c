aecfdcee63709ded8d5760f108faf53e757f6c26
lihao <lihao@sharkgulf.com>
2020-07-23 13:54:12 +0800

    添加数据结构 数组 、动态申请内存

--- C++/ShibalnuC++/main.c
+++ C++/ShibalnuC++/main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+//引入头文件  #include <xx.h>
 
 void NdkCode1();
 
@@ -39,7 +44,75 @@ void NdkCode1(){
     double testDouble = 1.666666;
     printf(" testsssLong : %.2lf  字节： %d\n",testDouble,sizeof(testDouble));
 
+
+
+    //字符串
+    char *str  = "今天天气好晴朗";
+    printf("*str %s \n",str);
+
+    //数组  容器
+    //数组是一块连续的内存空间
+    char strChar[200];
+    sprintf(strChar,"处处好风光 %d \n",999);
+    printf("%s \n",strChar);
+
+    printf("----------遍历数组--start------------\n");
+
+    int intArray[] = {1,3,2,5,4};
+    //遍历
+    //sizeof(test) 获取数组的字节 然后除以 单个类型占用的字节
+    for (int i = 0; i < (sizeof(intArray)/ sizeof(int)); ++i) {
+        printf("%d \n",intArray[i]);
+    }
+    printf("----------遍历数组--end------------\n");
+    //1个int 占用4个字节
+
+    //打印内存地址
+    printf("数组长度:%d\n",sizeof(intArray));
+    printf("数组+0:%#x\n",intArray + 0);
+    printf("数组+1:%\n",intArray + 1);
+
+    //* 取出内存地址里面对应的值
+    // 理解数组是一个连续的内存空间的例子
+    printf("*数组+0:%d\n",* (intArray + 0));
+    printf("*数组+1:%d\n",* (intArray + 1));
+    printf("*数组+2:%d\n",* (intArray + 2));
+
+    //%x   加一个# 代表16进制显示
+    //数组+0:0XE3FB79F0
+    //数组+1:0XE3FB79F4
+    //数组+2:0XE3FB79F8
+    //可以证明是连续的内存
+    //数组内存地址 和 数组首位内存地址 一样
+    printf("数组+0:%#X\n",(intArray + 0));
+    printf("数组+1:%#X\n",(intArray + 1));
+    printf("数组+2:%#X\n",(intArray + 2));
+
+
+    //c 面向过程， 没有面向对象 没有class
+    //c++ 面向对象  和java一摸一样 有class
+
+
+
+    // TODO 动态内存申请 （堆区）
+    //例子  声明都是在 栈区
+    int *p1; //悬空指针
+    long *p2; //悬空指针
+
+    //动态申请时 内部回去检测 是否有可以用的内存，里面可能会存在垃圾数据
+    int *p3 = calloc(10,sizeof(int));
+    //初始化 清空
+    memset(p3,NULL,10 * sizeof(int));
+
+    //释放
+    free(p3);
+
+    //释放之后 指针就成了野指针
+    p3 = 0; //p3 = NULL; 效果一样
 }
+
+
+
 
 
 
