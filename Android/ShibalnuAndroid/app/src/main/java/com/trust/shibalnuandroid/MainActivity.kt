2eb8d8da0fdd564b497ebea15edcc8bb95c3afb4
耐看的李先生 <zouzou1111@qq.com>
2020-09-03 20:05:34 +0800

    add Java

--- Android/ShibalnuAndroid/app/src/main/java/com/trust/shibalnuandroid/MainActivity.kt
+++ Android/ShibalnuAndroid/app/src/main/java/com/trust/shibalnuandroid/MainActivity.kt
@@ -2,11 +2,35 @@ package com.trust.shibalnuandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+import java.util.concurrent.Delayed
+import kotlin.concurrent.thread
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val job = Job()
+        CoroutineScope(job) .launch {
+                Test()
+        }
+    }
 
+
+    suspend fun Test(){
+
+        val s = suspendCoroutine<String> {
+            val block = {msg:String->
+                it.resume(msg)
+            }
+            Thread.sleep(1000 *3)
+            block("我是返回值")
+
+        }
+
+        Log.d("lhh","sss:${s}")
     }
 }
