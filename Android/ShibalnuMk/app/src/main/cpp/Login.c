cc04507e3cd1117c01dd43ac6fc18ad7ae3bfee1
23yearold-shibainu <1113690788@qq.com>
2020-12-08 19:03:39 +0800

    update Android Mk Project

--- Android/ShibalnuMk/app/src/main/cpp/Login.c
+++ Android/ShibalnuMk/app/src/main/cpp/Login.c
@@ -7,8 +7,8 @@ extern int get();
 
 JNIEXPORT jint JNICALL Java_com_shibalnu_shibalnumk_test_getNum
         (JNIEnv * env, jobject inst){
-
-    __android_log_print(ANDROID_LOG_DEBUG,"Trust","Test %d",get());
-    return 1;
+    jint num = get();
+    __android_log_print(ANDROID_LOG_DEBUG,"Trust","Test %d",num);
+    return num;
 }
 
--- Android/ShibalnuMk/app/src/main/java/com/shibalnu/shibalnumk/MainActivity.kt
+++ Android/ShibalnuMk/app/src/main/java/com/shibalnu/shibalnumk/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shibalnu.shibalnumk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        test().num
+        show_tv.text = "${test().num}"
     }
 
     external fun get():Int
